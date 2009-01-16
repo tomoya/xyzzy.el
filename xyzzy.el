0c52918e0687710afa6f82e1aad2055fa9572c5b
tomoya <tomoya.ton@gmail.com>
2009-01-16 13:57:54 +0900

    正規表現が正しくなかったのを修正。あと、コメントを付けた。

--- xyzzy.el
+++ xyzzy.el
@@ -1,14 +1,20 @@
 ;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 (defun skip-white-forward ()
-  (skip-chars-forward "\s " ))
+  "空白文字を前方にスキップします
+シンタックステーブル上でwhitespaceとして規定されている文字を前方にスキップします。"
+  (skip-chars-forward "\(\s \\|\t\)"))
 
 (defun skip-white-backward ()
-  (skip-chars-backward "\s "))
+  "空白文字を後方にスキップします。
+シンタックステーブル上でwhitespaceとして規定されている文字を後方にスキップします。"
+  (skip-chars-backward "\(\s \\|\t\)"))
 
 (defun goto-eol ()
+  "行末へ移動します。 (end-of-line) と同じ。"
   (end-of-line))
 
 (defun goto-bol ()
+  "行頭へ移動します。 (beginning-of-line) と同じ。"
   (beginning-of-line))
 
 
